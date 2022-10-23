5961747c258a9291a1fed70070a5e112b5b68daf
Nobody_Nowhere <63668759+rand12345@users.noreply.github.com>
2022-10-23 23:38:35 +0100

    fix broken merge

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use embedded_svc::{http::Headers, wifi::*};
+use embedded_svc::http::server::registry::Registry;
+use embedded_svc::http::server::{Request, Response};
+use embedded_svc::http::Headers;
+use embedded_svc::io::adapters::ToStd;
 use esp_idf_hal::mutex::{Condvar, Mutex};
-use esp_idf_svc::{netif, nvs::EspDefaultNvs, sysloop, wifi::EspWifi};
+use esp_idf_svc::http::server::EspHttpServer;
+use esp_idf_svc::netif::EspNetifStack;
+use esp_idf_svc::nvs::{self, EspDefaultNvs};
+use esp_idf_svc::nvs_storage::EspNvsStorage;
+use esp_idf_svc::sysloop::EspSysLoopStack;
+use esp_idf_svc::wifi::EspWifi;
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys::{self as _};
 
+use crate::configuration::{AppConfiguration, NvsStruct};
+use lazy_static::lazy_static;
 use log::*;
 use std::env;
 use std::io::Read;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-
-mod http_static;
-
+mod configuration;
+mod ota;
+mod wifi_init;
 #[macro_use]
 extern crate dotenv_codegen;
 
@@ -26,7 +36,14 @@ const HTMLINDEX: &str = include_str!("html/index.html");
 
 const WIFI_SSID_KEY: &str = dotenv!("WSSID");
 const WIFI_PASS_KEY: &str = dotenv!("WPASS");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AP_SSID_KEY: &str = dotenv!("APSSID");
+const AP_PASS_KEY: &str = dotenv!("APPASS");
+
+use std::sync::RwLock;
+
+lazy_static! {
+    static ref APP_CONFIG: RwLock<AppConfiguration> = RwLock::new(AppConfiguration::default());
+}
 
 fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -38,13 +55,31 @@ fn main() -> anyhow::Result<()> {
         EspNvsStorage::new_default(nvs.clone(), "config", true).unwrap(),
     ));
 
-    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid: WIFI_SSID_KEY.into(),
-        password: WIFI_PASS_KEY.into(),
-        ..Default::default()
-    }))?;
-    // wait for wifi
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    if let Ok(mut app_config) = APP_CONFIG.write() {
+        if let Err(e) = app_config.init(nvs_storage.clone()) {
+            panic!("{e}");
+        };
+        if let Some(val) = app_config.ap.channel {
+            app_config.ap.channel = Some(val + 1);
+            app_config.ap.write_to_nvs(&nvs_storage.clone())?;
+        } else {
+            app_config.ap.channel = Some(1);
+            app_config.ap.write_to_nvs(&nvs_storage.clone())?;
+        }
+    }
+    #[allow(unused)]
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    #[allow(unused)]
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+
+    let (sta, ap) = if let Ok(app_config) = APP_CONFIG.read() {
+        (app_config.sta.to_owned(), app_config.ap.to_owned())
+    } else {
+        panic!()
+    };
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, nvs)?);
+    let wifi_scan = wifi_init::scan(&mut wifi);
+    wifi = wifi_init::wifi(wifi, sta, ap)?;
 
     let mutex = Arc::new((Mutex::new(None), Condvar::new()));
     let _httpd = httpd(mutex)?;
@@ -61,148 +96,69 @@ fn main() -> anyhow::Result<()> {
 fn httpd(_mutex: Arc<(Mutex<Option<u32>>, Condvar)>) -> anyhow::Result<EspHttpServer> {
     let mut server = EspHttpServer::new(&Default::default())?;
 
-trait Multipart {
-    fn get_boundary(&self) -> Option<&str>;
-}
-impl Multipart for esp_idf_svc::http::server::EspHttpRequest<'_> {
-    fn get_boundary(&self) -> Option<&str> {
-        match self.header("Content-Type") {
-            Some(b) => {
-                let r: Vec<&str> = b.split('=').collect();
-                if r.len() == 2 {
-                    Some(r[1])
-                } else {
-                    eprint!("Error: Boundary string = {b}");
-                    None
-                }
-            }
-            None => {
-                eprint!("Error: No Boundary string");
-                None
-            }
-        }
-    }
-}
-
-fn httpd(
-    _mutex: Arc<(Mutex<Option<u32>>, Condvar)>,
-) -> anyhow::Result<esp_idf_svc::http::server::EspHttpServer> {
-    use embedded_svc::http::server::registry::Registry;
-    use embedded_svc::http::server::{Request, Response};
-    use embedded_svc::io::Read;
-
-    let mut server = esp_idf_svc::http::server::EspHttpServer::new(&Default::default())?;
-
     server
-        .handle_get("/", |_req, resp| {
-            resp.send_str("Hello from Rust!")?;
+        .handle_get("/id", |_req, resp| {
+            resp.send_str("OTATest")?;
             Ok(())
         })?
-        .handle_get("/ota", |_req, resp| {
-            // resp.send_str(http_static::_SERVER_OFFLINE)?; // Use for AP mode
-            resp.send_str(http_static::_SERVER_ONLINE)?; // Use for STA mode
+        .handle_get("/", move |_req, resp| {
+            // resp.send_str(&wifi_status)?;
+            resp.send_str(HTMLINDEX)?;
+            Ok(())
+        })?
+        .handle_get("/json", move |_req, resp| {
+            let config = APP_CONFIG.read().unwrap().to_owned();
+            if let Ok(payload) = serde_json::to_string(&config) {
+                resp.send_bytes(payload.as_bytes())?;
+            }
+            Ok(())
+        })?
+        .handle_get("/restart", |_req, _resp| {
+            info!("Restart requested");
+            unsafe { esp_idf_sys::esp_restart() } // no execution beyond this point
+            Ok(())
+        })?
+        .handle_get("/settings", |_req, resp| {
+            resp.send_str(HTMLSETTINGS)?;
             Ok(())
         })?
         .handle_post(
-            "/otaupload",
+            "/settings",
             |mut req, resp| -> Result<(), embedded_svc::http::server::HandlerError> {
-                let mut ota = esp_ota::OtaUpdate::begin()?;
-                let start_time = std::time::Instant::now();
-                let mut ota_bytes_counter = 0;
-                let mut multipart_bytes_counter = 0;
+                let mut body = Vec::new();
 
-                let len = {
-                    match req.content_len() {
-                        Some(val) => {
-                            if val == 0 {
-                                return Err(anyhow::anyhow!("Multipart POST len = 0").into());
-                            } else {
-                                val.to_owned()
-                            }
-                        }
-                        None => return Err(anyhow::anyhow!("Multipart POST len = 0").into()),
-                    }
-                };
-                let boundary = {
-                    match req.get_boundary() {
-                        Some(val) => val.to_owned(),
-                        None => return Err(anyhow::anyhow!("No boundary string").into()),
-                    }
-                };
+                ToStd::new(req.reader()).read_to_end(&mut body)?;
 
-                let mut buf = [0u8; 2048];
+                let ssid = url::form_urlencoded::parse(&body)
+                    .filter(|p| p.0 == "ssid")
+                    .map(|p| p.1.to_string())
+                    .next()
+                    .unwrap();
+                let pass = url::form_urlencoded::parse(&body)
+                    .filter(|p| p.0 == "pass")
+                    .map(|p| p.1.to_string())
+                    .next()
+                    .unwrap();
 
-                info!("Type: {:?} Len: {} bytes", &boundary, len);
+                
 
-                while let Ok(bytelen) = req.reader().read(&mut buf) {
-                    if start_time.elapsed() > Duration::from_millis(900) {
-                        std::thread::sleep(Duration::from_millis(10)) //wdt
-                    }
-                    if bytelen == 0 {
-                        break;
-                    }
-                    multipart_bytes_counter += bytelen;
+                resp.send_str(
+                &format!("Wifi setup completed {} {}, please reconnect to your home wifi to continue installation", ssid, pass),
+            )?;
 
-                    let payload = extract_payload(&buf[..bytelen], &boundary);
-
-                    ota_bytes_counter += &payload.len();
-
-                    match ota.write(payload) {
-                        Ok(_) => {
-                            info!(
-                                "Upload: {}%",
-                                (multipart_bytes_counter as f32 / len as f32) * 100.0
-                            )
-                        }
-                        Err(e) => {
-                            println!("Error! {e}\n{:02x?}", payload);
-                            resp.send_str(&format!(
-                                "<h1>Flashed {} bytes {}% FAILED</h1><br><br><p>{:02x?}</p>",
-                                multipart_bytes_counter,
-                                multipart_bytes_counter as f32 / len as f32 * 100.0,
-                                payload
-                            ))?;
-                            return Ok(());
-                        }
-                    }
-                }
-
-                match ota.finalize() {
-                    Ok(mut completed_ota) => {
-                        info!("Flashed {} bytes OK", ota_bytes_counter);
-
-                        completed_ota.set_as_boot_partition()?;
-                        info!("Set as boot partition - restarting");
-
-                        // this panics due to WIFI issue - also hangs browser in AP mode
-                        // thread '<unnamed>' panicked at 'ESP-IDF ERROR: ESP_ERR_WIFI_NOT_STARTED',
-                        completed_ota.restart();
-                    }
-                    Err(e) => {
-                        resp.send_str(&format!(
-                            "<h1>Flashed {} bytes FAILED</h1><br><br><p>{e}</p>",
-                            len,
-                        ))?;
-                    }
-                };
                 Ok(())
+            },
+        )?
+        .handle_get("/ota", |_req, resp| {
+            resp.send_str(HTMLOTA)?;
+            Ok(())
+        })?
+        // *********** OTA POST handler
+        .handle_post(
+            "/ota",
+            |req, resp| -> Result<(), embedded_svc::http::server::HandlerError> {
+                ota::ota_processing(req, resp)
             },
         )?;
     Ok(server)
-}
-
-fn extract_payload<'a>(buf: &'a [u8], boundary: &'a str) -> &'a [u8] {
-    if twoway::find_bytes(buf, boundary.as_bytes()).is_none() {
-        return buf;
-    }
-    let left_offset = match twoway::find_bytes(buf, &[13, 10, 13, 10]) {
-        Some(v) => v + 4,
-        None => 0,
-    };
-
-    let right_offset = match twoway::rfind_bytes(buf, &[13, 10, 45, 45]) {
-        Some(v) => v,
-        None => buf.len(),
-    };
-    &buf[left_offset..right_offset]
 }
